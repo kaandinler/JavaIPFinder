403ab3b13f6dbe33e6e654a5bb298030175222ac
kaan_ <kaan_d_tr@hotmail.com>
2020-10-21 16:40:42 +0300

    some error are fixed

--- IPFinder/src/com/kaandinler/IPFinder.java
+++ IPFinder/src/com/kaandinler/IPFinder.java
@@ -17,14 +17,45 @@ public class IPFinder extends JFrame implements ActionListener {
     private JLabel jLabel;
     private JTextField jTextField;
     private JButton jButton;
-    JLabel jResult;
+    private JLabel jResult;
+
+    private Color foundColor;
 
     //Constructor
-    public IPFinder() throws Exception {
+    public IPFinder(){
         // we can invoke a super method in a constructor method, so we did not type that in settingUpJFrame method.
         super("IP Finder"); // Title of the app
         //
-        settingUpJFrame();
+
+        //start construction of the jFrame.
+
+        jLabel = new JLabel("Enter website URL:");
+        jLabel.setBounds(50,75,150,20);
+
+        jResult = new JLabel("");
+        jResult.setBounds(50,170, 250,20);
+
+        jTextField = new JTextField();
+        jTextField.setBounds(50,100,200,20);
+        jTextField.setToolTipText(".com is auto added");
+
+        jButton = new JButton("Find IP");
+        jButton.setBounds(50,130, 200,30);
+        jButton.addActionListener(this);
+
+        //Adding components
+        add(jLabel);
+        add(jTextField);
+        add(jButton);
+        add(jResult);
+
+        setSize(310,300); // setting width and height of the frame
+        setLocationRelativeTo(null); // setting position of the frame
+        setLayout(null);
+        setVisible(true);
+        //end construction of the jFrame.
+
+        foundColor = new Color(0x4E89AE);
     }
 
     public void setUrl(String url){
@@ -48,52 +79,33 @@ public class IPFinder extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent actionEvent) {
 
         try {
-            //when button is clicked, String that is written in the textfield is sent to url variable;
-            setUrl(jTextField.getText());
+            //include .com if it hasn't included.
+            String url = jTextField.getText().trim().toLowerCase();
+            if(!url.endsWith(".com")){
+                url += ".com";
+            }else if(url.contains("\\s+")){
+                url = url.replace("\\s+","");
+            }
+            //set url
+            setUrl(url);
 
             InetAddress inetAddress = InetAddress.getByName(getUrl());
             IP4Address = inetAddress.getHostAddress();
             hostName = inetAddress.getHostName();
 
             //Showing ip address and host name that are written.
+            jResult.setForeground(foundColor);
             jResult.setText(getHostName() + " : " + getIP4Address());
 
             }catch (UnknownHostException e) {
                 e.printStackTrace();
-                JOptionPane.showMessageDialog(null,e.getMessage());
-            }catch (Exception e){
+                jResult.setForeground(Color.RED);
+                jResult.setText("No such a website!");
+            }
+            catch (Exception e){
+                e.printStackTrace();
                 JOptionPane.showMessageDialog(null,e.getMessage());
             }
 
     }
-
-    // jframe screen is constructed by this method
-    private void settingUpJFrame(){
-
-        jLabel = new JLabel("Enter website URL:");
-        jLabel.setBounds(50,70,150,20);
-
-        jResult = new JLabel("");
-        jResult.setBounds(50,170, 250,20);
-
-        jTextField = new JTextField();
-        jTextField.setBounds(50,100,200,20);
-
-        jButton = new JButton("Find IP");
-        jButton.setBounds(50,130, 200,30);
-        jButton.addActionListener(this);
-
-        //Adding component to JFrame that are created
-        add(jLabel);
-        add(jTextField);
-        add(jButton);
-        add(jResult);
-
-        setSize(400,300); // setting width and height of the frame
-        setLocationRelativeTo(null); // setting position of the frame
-        setLayout(null);
-        setVisible(true);
-    }
-
-
 }
